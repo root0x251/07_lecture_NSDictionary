49cf26e0ed00b67209e6af1d4597f34fc6bebc7a
Slava <rtootik@gmail.com>
2016-12-20 13:28:49 +0300

    Initial Commit

---
+++ 07_lecture_NSDictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  07_lecture_NSDictionary
+//
+//  Created by Slava on 20.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 07_lecture_NSDictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  07_lecture_NSDictionary
+//
+//  Created by Slava on 20.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
